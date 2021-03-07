6e973bd7079808c97ba235f2e83d1dd999a9bbbf
root <cagojeiger@naver.com>
2021-03-07 16:47:50 +0000

    edit case3_2 pull

--- example/ex_case_3(bulk_update)/ex_case_3_2.py
+++ example/ex_case_3(bulk_update)/ex_case_3_2.py
@@ -13,44 +13,40 @@ from models.case_3 import BaseCase
 
 if __name__ == '__main__':
     '''
-    아래와 같이 $unset으로 list 내에 있는 embeded document를 지우면
-    그 필드가 실제 db에 null로 치환이 되고, 이에 따라서 mongoengine에서
-    데이터를 읽어 올 때 에러가 걸린다
-
-    TODO: null로 치환이 안되게 하는 방법을 찾아보자!!
+    data setting time: 3.987682580947876
+    bulk update at mongoengine: 6.604163885116577
+    bulk update using aggregate: 0.028783321380615234
+    약 280배 속도 향상
     '''
     t = time.time()    
     BaseCase.make_dataset()
     print(f"data setting time: {time.time() - t}")
 
-    # t = time.time()    
-    # for obj in BaseCase.objects:
-    #     for o in obj.d:
-    #         if o.y == '5-5-y':
-    #             o.y = "edited"
-    #     obj.save()
-    # print(f"bulk update at mongoengine: {time.time() - t}")
+    t = time.time()    
+    for obj in BaseCase.objects:
+        for idx in range(len(obj.d)-1, -1, -1):
+            if '5-5' in obj.d[idx].y:
+                del obj.d[idx]
+        obj.save()
+    print(f"bulk update at mongoengine: {time.time() - t}")
 
     #######################################################
 
     pipe_match = {}
     pipe_set = {
-        "$unset": {
-            "d.$[idx]": 1
+        "$pull": {
+            "d": {
+                "y": re.compile('4-5', re.IGNORECASE)
+            }
         }
     }
-    array_filters = [
-        {"idx.y": re.compile('5-5-y', re.IGNORECASE)}
-    ]
 
     t = time.time()
     # arrayFilter를 쓸 경우 pipe_set에는 반드시 dict로 해야한다
     # 아마도 하나의 set만 가능한 것 같다
     obj = BaseCase._get_collection().update_many(
         pipe_match,
-        pipe_set,
-        upsert=True,
-        array_filters=array_filters
+        pipe_set
     )
     print(f"bulk update using aggregate: {time.time() - t}")
 
