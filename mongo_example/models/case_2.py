6fc0d52e85812251a25050d046401503b90f9ea3
root <cagojeiger@naver.com>
2021-03-13 04:29:07 +0000

    add case2 match filter

--- mongo_example/models/case_2.py
+++ mongo_example/models/case_2.py
@@ -1,7 +1,11 @@
 from typing import Type
+from datetime import datetime, timedelta
 
 from mongoengine import Document
-from mongoengine.fields import StringField, LazyReferenceField
+from mongoengine.fields import StringField, LazyReferenceField, DateTimeField
+
+
+DEFAULT_NOW = datetime(2000, 1, 1, 0, 0, 0)
 
 
 class BaseCase1:
@@ -28,14 +32,28 @@ class Case1_1(Document, BaseCase1):
 
 class Case1_2(Document, BaseCase2):
     d = LazyReferenceField(Case1_1)
+    e = DateTimeField()
 
     def __str__(self):
-        return f"a: {self.a}, b: {self.b}, c: {self.c}, d: {self.d}"
+        return f"a: {self.a}, b: {self.b}, c: {self.c}, d: {self.d}, e: {self.e}"
 
 
-def make_dateset(model_1: Type[Case1_1], model_2: Type[Case1_2]):
+def make_dateset(
+    model_1: Type[Case1_1],
+    model_2: Type[Case1_2]
+) -> None:
     model_1.objects.delete()
     model_2.objects.delete()
     for i in range(10000):
-        obj = model_1.objects.create(a=f"1-a-{i}", b=f"1-b-{i%10}", c=f"1-c-{i%100}")
-        model_2.objects.create(a=f"2-a-{i}", b=f"2-b-{i%10}", c=f"2-c-{i%100}", d=obj)
+        obj = model_1.objects.create(
+            a=f"1-a-{i}",
+            b=f"1-b-{i%10}",
+            c=f"1-c-{i%100}"
+        )
+        model_2.objects.create(
+            a=f"2-a-{i}",
+            b=f"2-b-{i%10}",
+            c=f"2-c-{i%100}",
+            d=obj,
+            e=DEFAULT_NOW + timedelta(days=i)
+        )
